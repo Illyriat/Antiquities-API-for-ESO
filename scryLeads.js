4b47d0a12a90d7bcf665bb802b1d62faa94d693a
James <j4m3s.robson@gmail.com>
2023-02-17 23:07:16 +0000

    Alikr Desert complete

--- scryLeads.js
+++ scryLeads.js
@@ -8,14 +8,134 @@ db.scryLeadInfo.insertMany([
     // Alik’r Desert
 
     {
-        "itemName" : "",
-        "quality" : "",
-        "level" : "",
-        "leadLocation" : "",
-        "type" : "",
-        "digZone" : "",
+        "itemName" : "Ancestral Orc Gloves",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Treasure Map Chest",
+        "type" : "Motif",
+        "digZone" : "Alik'r Desert",
         "img" : "",
-    }
+    },
+
+    {
+        "itemName" : "Antique Map of the Alik'r Desert",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Found at Inn Merchants (Requires Pathfinder Achievment)",
+        "type" : "Furnishing",
+        "digZone" : "Alikr'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Satakal Skinrazer",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Free Starter Lead",
+        "type" : "Treasure",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Dwarven Spine Coupling",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "“Curious Dwemer Rubble” in a corner of Rkulftzel Crafting Location (Song of Lamae Set)",
+        "type" : "Mount",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Na-Totambu Two-Fingered Gauntlet",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "Reward for excavating Satakal Skinrazer",
+        "type" : "Treasure",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Weathered Dwarven Cogs",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "Aldunz Delve Boss reward, Thinks-in-Gears",
+        "type" : "Mount",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Ancestral Orc Swords",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Treasure Map Chests",
+        "type" : "Motif",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Hegathe Divining Rod",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Reward for exacavating Na-Totambu blue lead",
+        "type" : "Treasure",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "The Heartland (Painting)",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Cyrodiil Chests, Cyrodiil Delve Bosses, Cyrodiil Cave Monsters, Treasure Maps in Cyrodiil, Monsters in Cyrodiil, Gati the Storm Sister in IC sewers",
+        "type" : "Furnishing",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Dwarven Leather Saddle Seat",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Volenfell Dungeon, Final Boss",
+        "type" : "Mount",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Torc Strand of Lore",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "World Bosses",
+        "type" : "Mythic Item",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Direnni Elegy Loop",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Coldrock Diggins, Delve Boss",
+        "type" : "Mythic Item",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Coil of Satakal",
+        "quality" : "Gold",
+        "level" : "Ultimate",
+        "leadLocation" : "Safebox's in Sentinel",
+        "type" : "Furnishing",
+        "digZone" : "Alik'r Desert",
+        "img" : "",
+    },
 
     // Artaeum
 
