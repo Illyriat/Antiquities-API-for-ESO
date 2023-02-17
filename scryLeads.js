c357a64a4d8cce6b23c965e8d62e1ef8676b1061
James <j4m3s.robson@gmail.com>
2023-02-17 22:29:50 +0000

    first commit. Blue print mapped out. Zones ordered. Object strcuture created.

---
+++ scryLeads.js
@@ -0,0 +1,131 @@
+// use scryLeads;
+// db.dropDatabase();
+
+db.scryLeadInfo.insertMany([
+
+    // Zone Leads. Alphabetical Order.
+
+    // Alik’r Desert
+
+    {
+        "itemName" : "",
+        "quality" : "",
+        "leadLocation" : "",
+        "type" : "",
+        "digZone" : "",
+        "img" : "",
+    }
+
+    // Artaeum
+
+
+    // Auridion
+
+
+    // Bal Foyen
+
+
+    // Bangkorai
+
+
+    // Betnikh
+
+
+    // Blackwood
+
+
+    // Bleackreack Isle
+
+
+    // Clockwork City
+
+
+    // Coldharbour
+
+
+    // Craglorn
+
+
+    // Deadlands
+
+
+    // Deshaan
+
+
+    // Eastmarch
+
+
+    // Eyevea
+
+
+    // Galen and Y'ffelon
+
+
+    // Glenumbra
+
+
+    // Gold Coast
+
+
+    // Grahtwood
+
+
+    // Greenshade
+
+
+    // Hew’s Bane
+
+
+    // High Isle and Amenos
+
+
+    // Khenarthi’s Roost
+
+
+    // Malabal Tor
+
+
+    // Murkmire
+
+
+    // Northern Elsweyr
+
+
+    // Reaper’s March
+
+
+    // Rivenspire
+
+
+    // Shadowfen
+
+
+    // Southern Elsweyr
+
+
+    // Stonefalls
+
+
+    // Stormhaven
+
+
+    // Stros M’Kai
+
+
+    // Summerset
+
+
+    // The Reach
+
+
+    // The Rift
+
+
+    // Vvardenfell
+
+
+    // Western Skyrim
+
+
+    // Wrothgar
+])
