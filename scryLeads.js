c7c13d2e4566b78d9407bd7d67bfa0db82d4fab3
James <j4m3s.robson@gmail.com>
2023-02-17 23:53:04 +0000

    Artaeum, Auridon, and Bal Foyen added

--- scryLeads.js
+++ scryLeads.js
@@ -139,15 +139,271 @@ db.scryLeadInfo.insertMany([
 
     // Artaeum
 
+    {
+        "itemName" : "Faded Psijic Folio",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Free Starter lead",
+        "type" : "Treasure",
+        "digZone" : "Artaeum",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Warped Scrying Dipper",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "Reward for excavating Faded Psijic Folio",
+        "type" : "Treasure",
+        "digZone" : "Artaeum",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Ritemaster's Slate",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Reward for excavating Warped Scrying Dipper",
+        "type" : "Treasure",
+        "digZone" : "Artaeum",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Coral Plating",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Artaeum fishing holes",
+        "type" : "Mythic Item",
+        "digZone" : "Artaeum",
+        "img" : "",
+    },
 
     // Auridion
 
+    {
+        "itemName" : "Ancestral High Elf Bows",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Treasure Map Chest",
+        "type" : "Motif",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Antique Map of Auridon",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Firsthold City, Overlook Inn, Barkeeper. Requires Pathfinder Achievment.",
+        "type" : "Furnishing",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Festival of Defiance Token",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Free Starter lead",
+        "type" : "Treasure",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Scale-Etched Slither Pipe",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "Reward for excavating Festival of Defiance Token",
+        "type" : "Treasure",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Aldmeri Golden Embellishments",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Thieves Trives in any zone",
+        "type" : "Aldmeri Symphonia Part",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Amcestral High Elf Chests",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Treasure Map Chest",
+        "type" : "Motif",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Aquiline Calian Sphere",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Reward for excavating Scale-Etched Slither Pipe",
+        "type" : "Treasure",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Maormeri Serpent (Painting)",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Safeboxes, Treasure Map Chests, Public Dungeon Boss, Random Monster, Thieves Troves",
+        "type" : "Furnishing",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Echoes of the Aldmeris (Painting)",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Chests, Pack Merchants Bags",
+        "type" : "Furnishing",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Inert Anemone",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "All World Bosses",
+        "type" : "Mythic Item",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Nilumin River Pearls",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Banished Cells 1, Final Boss",
+        "type" : "Mythic Item",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Aetherquartz Prayer Beads",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Ondil Auridon Delve Boss",
+        "type" : "Mythic Item",
+        "digZone" : "Auridon",
+        "img" : "",
+    },
 
     // Bal Foyen
 
+    {
+        "itemName" : "Ancestral Nord Belts",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Treasure Map Chest",
+        "type" : "Motif",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Basalt-Table Game Set",
+        "quality" : "Green",
+        "level" : "Basic",
+        "leadLocation" : "Free Starter lead",
+        "type" : "Treasure",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Ancestral Nord Staves",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "Treasure Map Chest",
+        "type" : "Motif",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Great House Gold Chalice",
+        "quality" : "Blue",
+        "level" : "Intermediate",
+        "leadLocation" : "Reward for excavating Basalt-Table Game Set",
+        "type" : "Treasure",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Resdayni Signet Ring",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Reward for excavating Great House Gold Chalice",
+        "type" : "Treasure",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "The Dutiful Guar (Painting)",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Safeboxes, Thieves Troves, Nodes",
+        "type" : "Furnishing",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Tri-Angled Truth Altar",
+        "quality" : "Purple",
+        "level" : "Advanced",
+        "leadLocation" : "Safeboxes, Treasure Chests",
+        "type" : "Furnishing",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Malacath's Brutal Rune Core",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Imperial City, District Bosses",
+        "type" : "Mythic Item",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
+
+    {
+        "itemName" : "Silverweave Cord",
+        "quality" : "Gold",
+        "level" : "Master",
+        "leadLocation" : "Covenant Enemies",
+        "type" : "Mythic Item",
+        "digZone" : "Bal Foyen",
+        "img" : "",
+    },
 
     // Bangkorai
 
+    {
+        "itemName" : "",
+        "quality" : "",
+        "level" : "",
+        "leadLocation" : "",
+        "type" : "",
+        "digZone" : "Bangkorai",
+        "img" : "",
+    },
 
     // Betnikh
 
@@ -155,7 +411,7 @@ db.scryLeadInfo.insertMany([
     // Blackwood
 
 
-    // Bleackreack Isle
+    // Bleackrock Isle
 
 
     // Clockwork City
